fe9efa53ea65647a346f0a5ba5c940864dbe525a
Link4Electronics <link4electronics@gmail.com>
2026-03-26 11:05:26 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,7 +10,7 @@ pacman -Syu --noconfirm mixxx
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
